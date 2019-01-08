3f16344db40f047b53bca18745df0be83ba5fc79
Robert Dominguez <dominguez.1st@gmail.com>
2019-01-07 18:48:11 -0700

    Messed with loops and functions

---
+++ condidtionals-and-functions.js
@@ -0,0 +1,78 @@
+/*
+* Conditionals, functions, scope, and Loops
+ */
+
+// Equals
+let equals = 1 === 1;
+console.log(equals);
+
+//Greater than
+let greaterThan = 5 > 1;
+
+//Less than
+let lessThan = 2 < 10;
+
+// Greater than Equals
+let greaterThanEq = 5 >=5;
+
+//Less Than Equals
+let lessThanEq = 4 <= 9;
+
+//Not Equals
+let notEquals  = 5 !== 2;
+
+
+let storeA = 4.40;
+let storeB = 4.40;
+
+let storeAIsLower = storeA < storeB;
+
+function compareStorePrices (storeA, storeB) {
+	let storeAIsLower = storeA < storeB;
+	if(storeAIsLower) {
+		console.log("Store A has a lower price.")
+	} else if(storeB < storeA) {
+		console.log("Store B has a lower price.")
+	} else {
+		console.log("Their prices are equal.")
+	}
+}
+
+compareStorePrices(10, 5);
+compareStorePrices(7, 10);
+
+function squareNum(number) {
+	return number * number;
+}
+
+let squareNumber = squareNum(4);
+console.log(squareNumber);
+
+let x = 10;
+
+function addNumbers (n, m, x) {
+	console.log(x);
+	let b;
+	if (1===1) {
+		console.log(b);
+		return n+ m;
+	}
+}
+addNumbers(1,2,10);
+
+let ourArray = [1, 2, 3, 4, 5, 6, 7];
+
+let arrLen =ourArray.length;
+for (let i = 0; i < arrLen; i++){
+	//console.log(" i is equal to: " + i);
+	console.log(ourArray [i]);
+	for (let j = 0; j < 5 ; j++){
+		console.log('j is equal to: ' + j);
+	}
+}
+
+let c = 0;
+while (c < 10) {
+	console.log('Ran');
+	c = c + 1;
+}
